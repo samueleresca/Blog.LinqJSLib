0deb228d9b703fd981050e12cb0ba86ef96c70b3
Samuele Resca <samuele.resca@gmail.com>
2015-06-20 14:48:54 +0200

    First commit

---
+++ Blog.LinqJSlib/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Blog.LinqJSlib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Blog.LinqJSlib")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e6b89c42-e909-4b2c-806a-ec82eb8ada13")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Blog.LinqJSlib/Scripts/tests.js
@@ -0,0 +1,71 @@
+﻿
+//REPO
+var objArray = [
+    {
+        id: 1,
+        description: "Cuba libre",
+        price: 6.00
+    },
+        {
+            id: 2,
+            description: "Beer",
+            price: 3.50
+        },
+            {
+                id: 3,
+                description: "Vodka Lemon",
+                price: 5.00
+            },
+            {
+                id: 4,
+                description: "Long island",
+                price: 5.50
+            },
+              {
+                  id: 5,
+                  description: "Caipiroska",
+                  price: 7.50
+              }
+
+]
+
+
+//SIMPLE QUERY
+
+var testResult1= Enumerable.From(objArray)
+    .Where(function (x) { return x.price >= 5.00 })
+.Select(function (x) { return x.description }).ToArray();
+
+
+QUnit.test("Test Where() and Select()", function (assert) {
+    assert.ok(testResult1[0] == "Cuba libre", "Passed!");
+});
+
+//LAMBDA EXP
+var testResult2 = Enumerable.From(objArray)
+    .Where("$.price >= 5.00")
+.Select("$.description").ToArray();
+
+QUnit.test("Test Where() and Select() using LAMBDA selector", function (assert) {
+    assert.ok(testResult2[0] == "Cuba libre", "Passed!");
+});
+
+
+
+//LAMBDA EXP
+var testResult3 = Enumerable.From(objArray)
+    .Where("$.price >= 5.00")
+    .OrderBy("$.price")
+.Select(function (x) {
+       return x.description
+}).ToArray();
+
+QUnit.test("Test ORDERBY() using LAMBDA selector", function (assert) {
+    assert.ok(testResult3[0] == "Vodka Lemon", "Passed!");
+});
+
+
+
+
+
+
